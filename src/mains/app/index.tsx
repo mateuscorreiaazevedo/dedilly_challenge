7874290c315655586784f9e6d5695b3f15682a6f
Mateus <mateuscorreiaazevedo@gmail.com>
2023-03-13 21:34:04 -0300

    feat: add lib toastfy on boilerplate

--- src/mains/app/index.tsx
+++ src/mains/app/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-export const App = () => {
+export function App () {
   return (
     <>
       <h1>Hello World</h1>
+      <ToastContainer />
     </>
   )
 }
