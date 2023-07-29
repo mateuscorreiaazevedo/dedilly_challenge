54fc63c1558d61eb8db53c3e6cc81e59eaf73da4
Mateus Azevedo <99230374+mateuscorreiaazevedo@users.noreply.github.com>
2023-07-28 23:21:02 -0300

    refactor: refact folders on app

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from '@/mains/app'
+import { App } from '@/components/app'
 import '@/assets/styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
